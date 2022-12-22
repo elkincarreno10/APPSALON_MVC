8d9ed31093f46fbb48d5ae165f0ecf534b1e7812
Elkin Carreño <elquin50000@gmail.com>
2022-12-22 12:17:00 -0500

    Deployment

---
+++ views/servicios/formulario.php
@@ -0,0 +1,6 @@
+<div class="campo">
+    <label for="nombre">Nombre</label>
+    <input type="text" id="nombre"  placeholder="Nombre Servicio" name="nombre" value="<?php echo $servicio->nombre; ?>">
+    <label for="precio">Precio</label>
+    <input type="number" id="precio"  placeholder="Precio Servicio" name="precio" value="<?php echo $servicio->precio; ?>">
+</div>
